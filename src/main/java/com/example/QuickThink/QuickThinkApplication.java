4fd18b89f99e2f77836b09654be042fecea53eb1
flora101 <flora8207@naver.com>
2023-04-07 15:45:43 +0900

    first commit

---
+++ src/main/java/com/example/QuickThink/QuickThinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickThink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickThinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickThinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuickThink/QuickThinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuickThink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickThinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
